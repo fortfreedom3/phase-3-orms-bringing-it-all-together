907938d5ac0aab96f53952fc6b2d3961eb075897
Morgan Byrne <79115095+fortfreedom3@users.noreply.github.com>
2022-01-02 11:27:50 -0500

    commit tree e563d8f6531d7343215f30aec9af58c4679b318d

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+    attr_accessor :name, :breed, :id
 
+    def initialize(name:, breed:, id: nil)
+        @id = id
+        @name = name
+        @breed = breed
+    end
+
+    def self.create_table 
+        create_table :dogs do |t|
+            t.integer :id
+            t.string :name
+            t.string :breed
+        end
+    end
 end
